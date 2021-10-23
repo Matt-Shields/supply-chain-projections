ad3a248561db5b732f16b0d56fdc53939874f73b
mshields <mshields@nrel.gov>
2021-10-23 11:13:57 -0600

    Revised excel read and loop through scenarios

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 group_rows = {'# of OSP': 'Delete',
               '# of export cables': 'Delete',
@@ -23,5 +24,94 @@ group_rows = {'# of OSP': 'Delete',
               'substructure: total jacket, #\n': 'Total number of foundations'
               }
 
+DNV_indices = {
+    'installMW': 0,
+    'projects': 1,
+    'turb': 2,
+    'turb12MW': 3,
+    'turb15MW': 4,
+    'turb18MW': 5,
+    'monopiles': 19,
+    'gbf': 21,
+    'jacketTurb': 22,
+    'jacketOSS': 23,
+    'semiTurb': 22,
+    'semiOSS': 23,
+    'array': 27,
+    'export': 28,
+    'wtiv': 43,
+    'barge': 46,
+    'ctv': 47,
+    'clv': 48,
+    'berths': 53,
+    '2022col': 3,
+}
 
 # def create_ITC_tables(df):
+def read_vars(file, sheet, xrange, header=81, cols='B:Q', rows=56, ind=DNV_indices):
+    df = pd.read_excel(file, sheet_name=sheet, header=header, usecols=cols, nrows=rows)
+    # Extract all required variables as numpy arrays
+    _installMW = df.iloc[ind['installMW'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _projects = df.iloc[ind['projects'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _turb = df.iloc[ind['turb'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _turb12MW = df.iloc[ind['turb12MW'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _turb15MW = df.iloc[ind['turb15MW'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _turb18MW = df.iloc[ind['turb18MW'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _array = df.iloc[ind['array'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _export = df.iloc[ind['export'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _wtiv = df.iloc[ind['wtiv'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _barge = df.iloc[ind['barge'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _ctv = df.iloc[ind['ctv'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    _clv = df.iloc[ind['clv'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+    _berths = df.iloc[ind['berths'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+    if 'WC' in sheet:
+        # Floating
+        _semiTurb = df.iloc[ind['semiTurb'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+        _semiOSS = df.iloc[ind['semiOSS'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+        _semi = np.add(_semiTurb, _semiOSS)
+        _out = {
+                'installMW': _installMW,
+                'projects': _projects,
+                'turb': _turb,
+                'turb12MW': _turb12MW,
+                'turb15MW': _turb15MW,
+                'turb18MW': _turb18MW,
+                'semiTurb': _semiTurb,
+                'semiOSS': _semiOSS,
+                'semi': _semi,
+                'array': _array,
+                'export': _export,
+                'wtiv': _wtiv,
+                'barge': _barge,
+                'ctv': _ctv,
+                'clv': _clv,
+                'berths': _berths
+            }
+    else:
+        _monopiles = df.iloc[ind['monopiles'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+        _jacketTurb = df.iloc[ind['jacketTurb'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+        _jacketOSS = df.iloc[ind['jacketOSS'], ind['2022col']:ind['2022col'] + len(xrange) - 1].to_numpy()
+        _jacket = np.add(_jacketTurb, _jacketOSS)
+        _gbf = df.iloc[ind['gbf'], ind['2022col']:ind['2022col']+len(xrange)-1].to_numpy()
+        _out = {
+                'installMW': _installMW,
+                'projects': _projects,
+                'turb': _turb,
+                'turb12MW': _turb12MW,
+                'turb15MW': _turb15MW,
+                'turb18MW': _turb18MW,
+                'monopiles': _monopiles,
+                'jacketTurb': _jacketTurb,
+                'jacketOSS': _jacketOSS,
+                'jacket': _jacket,
+                'gbf': _gbf,
+                'array': _array,
+                'export': _export,
+                'wtiv': _wtiv,
+                'barge': _barge,
+                'ctv': _ctv,
+                'clv': _clv,
+                'berths': _berths
+            }
+
+    return _out
--- main.py
+++ main.py
@@ -3,37 +3,54 @@
 import numpy as np
 import pandas as pd
 import plot_routines as pr
-from helpers import group_rows
+from helpers import group_rows, read_vars#, DNV_indices
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    # Import CSVs with 30 GW pipeline
+    # Define input spreadsheet
     DNV_gantt = '4.3 NREL Supply Chain Study-for NREL.v2.xlsm'
-    DNV_gantt_v2 = '4.3 NREL Supply Chain Study-for NREL.v3.xlsx'
-    #other_gantt = 'All Scenarios_Varied LC_Jobs.xlsx'#'BAU_Floating_Gantt.xlsx'
-    fixed_pipeline_30GW_raw = pd.read_excel(DNV_gantt_v2, sheet_name='Aggregated', header=4, usecols='B:W', nrows=30,) #should header = none?
-    #fixed_pipeline_BAU = pd.read_excel(DNV_gantt, sheet_name='Aggregated', dtype=object, header=4, usecols='BD:BR',index_col='WC-UNDEVELOPED',)
-    float_pipeline_raw = pd.read_excel(DNV_gantt_v2, sheet_name='Aggregated', header=36, usecols='B:W',nrows=16,)
-    #float_pipeline = pd.read_excel(DNV_gantt, sheet_name='Aggregated', header=4, usecols='BD:BR',index_col='WC-UNDEVELOPED',)
-    fixed_pipeline_30GW = pd.pivot_table(fixed_pipeline_30GW_raw, index='Project COD', aggfunc=np.sum).transpose().drop(['PROJECTS', 'EC -UNDEVELOPED'], axis=1)
-    float_pipeline = pd.pivot_table(float_pipeline_raw, index='Project COD', aggfunc=np.sum).transpose().drop(['WC -UNDEVELOPED'], axis=1)
+    # Define scenarios to plot
+    scenarios = ['EC-UNCONSTR', 'WC-UNC']
+    # Define date range
+    CODstart = 2022
+    CODend = 2030
+    COD_years = np.arange(CODstart, CODend+1)
+    # Loop through scenarios
+    pipeline = {}
+    for s in scenarios:
+        # Read in Excel
+        pipeline[s] = read_vars(file=DNV_gantt, sheet=s, xrange=COD_years)
 
-    #print(fixed_pipeline_30GW)
-    #print(float_pipeline)
-    # Stacked bar charts of fixed and floating with cumulative
-    COD_years = fixed_pipeline_30GW.columns.to_numpy()
-    #print(COD_years)
-    # Set max year if desired
-    xmax = 2030
-    try:
-        # Set max COD on x axis
-        COD_years = np.extract(COD_years < (xmax+1), COD_years)
-        fixed_pipeline_30GW = fixed_pipeline_30GW[list(np.extract(COD_years < (xmax+1), COD_years))]
-        #fixed_pipeline_BAU = fixed_pipeline_BAU[list(np.extract(COD_years < (xmax+1), COD_years))]
-        float_pipeline = float_pipeline[list(np.extract(COD_years < (xmax+1), COD_years))]
-    except NameError:
-        # xmax not defined
-        pass
+
+    # # Import CSVs with 30 GW pipeline
+    #     # DNV_gantt = '4.3 NREL Supply Chain Study-for NREL.v2.xlsm'
+    #     # DNV_gantt_v2 = '4.3 NREL Supply Chain Study-for NREL.v3.xlsx'
+    #     # #other_gantt = 'All Scenarios_Varied LC_Jobs.xlsx'#'BAU_Floating_Gantt.xlsx'
+    #     # fixed_pipeline_30GW_raw = pd.read_excel(DNV_gantt_v2, sheet_name='Aggregated', header=4, usecols='B:W', nrows=30,) #should header = none?
+    #     # #fixed_pipeline_BAU = pd.read_excel(DNV_gantt, sheet_name='Aggregated', dtype=object, header=4, usecols='BD:BR',index_col='WC-UNDEVELOPED',)
+    #     # float_pipeline_raw = pd.read_excel(DNV_gantt_v2, sheet_name='Aggregated', header=36, usecols='B:W',nrows=16,)
+    #     # #float_pipeline = pd.read_excel(DNV_gantt, sheet_name='Aggregated', header=4, usecols='BD:BR',index_col='WC-UNDEVELOPED',)
+    #     # fixed_pipeline_30GW = pd.pivot_table(fixed_pipeline_30GW_raw, index='Project COD', aggfunc=np.sum).transpose().drop(['PROJECTS', 'EC -UNDEVELOPED'], axis=1)
+    #     # float_pipeline = pd.pivot_table(float_pipeline_raw, index='Project COD', aggfunc=np.sum).transpose().drop(['WC -UNDEVELOPED'], axis=1)
+    #     #
+    #     # #print(fixed_pipeline_30GW)
+    #     # #print(float_pipeline)
+    #     # # Stacked bar charts of fixed and floating with cumulative
+    #     # COD_years = fixed_pipeline_30GW.columns.to_numpy()
+    #     # #print(COD_years)
+    #     # # Set max year if desired
+    #     # xmax = 2030
+    #     # try:
+    #     #     # Set max COD on x axis
+    #     #     COD_years = np.extract(COD_years < (xmax+1), COD_years)
+    #     #     fixed_pipeline_30GW = fixed_pipeline_30GW[list(np.extract(COD_years < (xmax+1), COD_years))]
+    #     #     #fixed_pipeline_BAU = fixed_pipeline_BAU[list(np.extract(COD_years < (xmax+1), COD_years))]
+    #     #     float_pipeline = float_pipeline[list(np.extract(COD_years < (xmax+1), COD_years))]
+    #     # except NameError:
+    #     #     # xmax not defined
+    #     #     pass
+
+
     # Total capacity
     # Single scenario
     yvals = [fixed_pipeline_30GW.loc['Total Project Capacity, MW'].to_numpy(), float_pipeline.loc['Total Project Capacity, MW'].to_numpy()]
@@ -43,7 +60,10 @@ if __name__ == '__main__':
     pr.stacked_bar_cumulative(COD_years, zip(yvals, colors, names), fname='30GW_deployment', y1max=10000)
 
     #Figure 3 econ gaps assessment
-    yvals_fig3 = [fixed_pipeline_30GW.loc['Total # of 12 MW turbines'].to_numpy(), [fixed_pipeline_30GW.loc['Total # of 15 MW turbines'].to_numpy(), [fixed_pipeline_30GW.loc['Total # of 18 MW turbines'].to_numpy()
+    yvals_fig3 = [fixed_pipeline_30GW.loc['Total # of 12 MW turbines'].to_numpy(),
+                  fixed_pipeline_30GW.loc['Total # of 15 MW turbines'].to_numpy(),
+                    fixed_pipeline_30GW.loc['Total # of 18 MW turbines'].to_numpy()
+                    ]
     colors_fig3 = ['#3D6321']
     names_fig3 = ['Fixed-bottom (low constrained)']
 

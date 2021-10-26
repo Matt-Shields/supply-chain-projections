96a3fe5d2de20cb93bc0ab31f81a18010f9263b1
Abigayle Moser <85366919+am0ser@users.noreply.github.com>
2021-10-26 11:36:56 -0600

    updated terminology for low and highly constrained scenarios

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ if __name__ == '__main__':
     #Nacelle Unconstrained East Coast
     yvals_nac = [jobsPipeline['Nacelle']['25domEC_UNC'], jobsPipeline['Nacelle']['100domEC_UNC']]
     colors_fte = [colors_list['fixed'], colors_list['float']]
-    names_fte = ['25% Domestic Content', '100% Domestic Content']
+    names_fte = ['25% Domestic Content, Unconstrained Supply Chain', '100% Domestic Content, Unconstrained Supply Chain']
     pr.line_plots2(dateYrs, zip(yvals_nac, colors_fte, names_fte), fname='Figs/Nacelle_Job_Requirements_ECUNC', ymax=30000)
 
     #Rotor Blades Unconstrained East Coast
@@ -328,9 +328,9 @@ if __name__ == '__main__':
 
     #Nacelle Low and High constrained scenarios
     yvals_nac_lh = [jobsPipeline['Nacelle']['25domEC_LOW'], jobsPipeline['Nacelle']['100domEC_LOW'], jobsPipeline['Nacelle']['25domEC_HIGH'], jobsPipeline['Nacelle']['100domEC_HIGH']]
-    colors_lh = [colors_list['fixed'], colors_list['float'], colors_list['fixed'], colors_list['float']]
-    names_lh = ['25% Domestic Content, Low Constrained', '100% Domestic Content, Low Constrained', '25% Domestic Content, High Constrained', '100% Domestic Content, High Constrained']
-    lines_lh = ['dashed', 'solid', 'dashed', 'solid']
+    colors_lh = [colors_list['wtiv'], colors_list['clv'], colors_list['wtiv'], colors_list['clv']]
+    names_lh = ['25% Domestic Content, Moderate Supply Constraints', '100% Domestic Content, Moderate Supply Constraints', '25% Domestic Content, Significant Supply Constraints', '100% Domestic Content, Significant Supply Constraints']
+    lines_lh = ['solid', 'solid', 'dashed', 'dashed']
 
     pr.line_plots4(dateYrs, zip(yvals_nac_lh, colors_lh, lines_lh, names_lh), fname='Figs/Nacelle_Job_Requirements_ECLH', ymax=30000)
 
@@ -344,12 +344,32 @@ if __name__ == '__main__':
 
     #Transition Piece Low and High East Coast
     yvals_tp_lh = [jobsPipeline['Transition Piece']['25domEC_LOW'], jobsPipeline['Transition Piece']['100domEC_LOW'], jobsPipeline['Transition Piece']['25domEC_HIGH'], jobsPipeline['Transition Piece']['100domEC_HIGH']]
-    pr.line_plots4(dateYrs, zip(yvals_tp_lh, colors_lh, lines_lh, names_lh), fname='Figs/Transition_Piece_Job_Requirements_ECLH', ymax=6000)
+    pr.line_plots4(dateYrs, zip(yvals_tp_lh, colors_lh, lines_lh, names_lh), fname='Figs/Transition_Piece_Job_Requirements_ECLH', ymax=5000)
 
-    #Rotor Blades Low and High East Coast
-    yvals_blades_lh = [jobsPipeline['Rotor Blades']['25domEC_LOW'], jobsPipeline['Rotor Blades']['100domEC_LOW'], jobsPipeline['Rotor Blades']['25domEC_HIGH'], jobsPipeline['Rotor Blades']['100domEC_HIGH']]
-    pr.line_plots4(dateYrs, zip(yvals_blades_lh, colors_lh, lines_lh, names_lh), fname='Figs/Nacelle_Job_Requirements_ECLH', ymax=6000)
+    #Jacket (For Turbine) Low and High East Coast
+    yvals_jacket_lh = [jobsPipeline['Jacket (For Turbine)']['25domEC_LOW'], jobsPipeline['Jacket (For Turbine)']['100domEC_LOW'], jobsPipeline['Jacket (For Turbine)']['25domEC_HIGH'], jobsPipeline['Jacket (For Turbine)']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_jacket_lh, colors_lh, lines_lh, names_lh), fname='Figs/Turbine_Jacket_Job_Requirements_ECLH', ymax=5000)
 
-    #Rotor Blades Low and High East Coast
-    yvals_blades_lh = [jobsPipeline['Rotor Blades']['25domEC_LOW'], jobsPipeline['Rotor Blades']['100domEC_LOW'], jobsPipeline['Rotor Blades']['25domEC_HIGH'], jobsPipeline['Rotor Blades']['100domEC_HIGH']]
-    pr.line_plots4(dateYrs, zip(yvals_blades_lh, colors_lh, lines_lh, names_lh), fname='Figs/Nacelle_Job_Requirements_ECLH', ymax=6000)
+    #Monopiles Low and High East Coast
+    yvals_mono_lh = [jobsPipeline['Monopiles']['25domEC_LOW'], jobsPipeline['Monopiles']['100domEC_LOW'], jobsPipeline['Monopiles']['25domEC_HIGH'], jobsPipeline['Monopiles']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_mono_lh, colors_lh, lines_lh, names_lh), fname='Figs/Monopiles_Job_Requirements_ECLH', ymax=8000)
+
+    #GBF Low and High East Coast
+    yvals_GBF_lh = [jobsPipeline['GBF']['25domEC_LOW'], jobsPipeline['GBF']['100domEC_LOW'], jobsPipeline['GBF']['25domEC_HIGH'], jobsPipeline['GBF']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_GBF_lh, colors_lh, lines_lh, names_lh), fname='Figs/GBF_Job_Requirements_ECLH', ymax=8000)
+
+    #Jacket (For Substation) and High East Coast
+    yvals_subj_lh = [jobsPipeline['Jacket (For Substation)']['25domEC_LOW'], jobsPipeline['Jacket (For Substation)']['100domEC_LOW'], jobsPipeline['Jacket (For Substation)']['25domEC_HIGH'], jobsPipeline['Jacket (For Substation)']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_subj_lh, colors_lh, lines_lh, names_lh), fname='Figs/Substation_Jacket_Job_Requirements_ECLH', ymax=80)
+
+    #Substation (Topside) Low and High East Coast
+    yvals_subt_lh = [jobsPipeline['Substation (Topside)']['25domEC_LOW'], jobsPipeline['Substation (Topside)']['100domEC_LOW'], jobsPipeline['Substation (Topside)']['25domEC_HIGH'], jobsPipeline['Substation (Topside)']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_subt_lh, colors_lh, lines_lh, names_lh), fname='Figs/Substation_Topside_Job_Requirements_ECLH', ymax=60)
+
+    #Array Cable Low and High East Coast
+    yvals_array_lh = [jobsPipeline['Array Cable']['25domEC_LOW'], jobsPipeline['Array Cable']['100domEC_LOW'], jobsPipeline['Array Cable']['25domEC_HIGH'], jobsPipeline['Array Cable']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_array_lh, colors_lh, lines_lh, names_lh), fname='Figs/Array_Cable_Job_Requirements_ECLH', ymax=2000)
+
+    #Export Cable Low and High East Coast
+    yvals_export_lh = [jobsPipeline['Export Cable']['25domEC_LOW'], jobsPipeline['Export Cable']['100domEC_LOW'], jobsPipeline['Export Cable']['25domEC_HIGH'], jobsPipeline['Export Cable']['100domEC_HIGH']]
+    pr.line_plots4(dateYrs, zip(yvals_export_lh, colors_lh, lines_lh, names_lh), fname='Figs/Export_Cable_Job_Requirements_ECLH', ymax=5000)
